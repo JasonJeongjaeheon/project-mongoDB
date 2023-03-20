54487b60d4234c43f2f2a3bea5f3508b1bfde888
정재헌 <jaeheonjason@gmail.com>
2023-03-20 18:22:04 +0900

    FIX: kakao login http method

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -4,6 +4,6 @@ import * as userController from '../controllers/userController'
 const routes: Router = express.Router();
 
 routes.post('/login', userController.userLogin)
-routes.get('/login/kakao', userController.kakaoLogin)
+routes.post('/login/kakao', userController.kakaoLogin)
 
 export { routes }
